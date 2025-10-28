ac40102f1a5a95364e2294a655f834a525cfd3fa
Joaquim Neto <netojoaquimjj@gmail.com>
2025-10-28 10:53:45 -0300

    Adicionando o código que faz o cálculo de autonomia da bateria

--- Pi1_G1_Firmware/src/main.cpp
+++ Pi1_G1_Firmware/src/main.cpp
@@ -3,30 +3,47 @@
 
 MonitorEnergia ina219;
 
-// put function declarations here:
-int myFunction(int, int);
+const float V_nominal = 6.0;
+const float Capacidade_Ah = 2.7;
+float EnergiaTotal;
+float EnergiaConsumida = 0;
+unsigned long ultimoTempo;
 
 void setup() {
-  // put your setup code here, to run once:
   Serial.begin(115200);
   delay(100);
 
   if (!ina219.iniciar()) {
     Serial.println("ERRO: Sensor INA219 não encontrado!");
-    while (1) {
-      delay(10);
-    }
+    while (1) delay(10);
   }
 
+  EnergiaTotal = V_nominal * Capacidade_Ah * 3600.0; // energia total em Joules
+  ultimoTempo = millis();
+
+  Serial.println("Monitor de energia iniciado!");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  float corrente = ina219.obterCorrente();
+  float corrente_mA = ina219.obterCorrente(); // leitura em mA
+  float corrente = corrente_mA / 1000.0;      // converte para A
   float tensao = ina219.obterTensao();
-}
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  unsigned long agora = millis();
+  float dt = (agora - ultimoTempo) / 1000.0; // segundos
+  ultimoTempo = agora;
+
+  float P = tensao * corrente; // potência em W
+  EnergiaConsumida += P * dt;  // energia em J acumulada
+
+  if (EnergiaConsumida > EnergiaTotal) EnergiaConsumida = EnergiaTotal;
+
+  float EnergiaRestante = EnergiaTotal - EnergiaConsumida;
+  float t_restante = (P > 0.001) ? (EnergiaRestante / P) : INFINITY;
+  if (t_restante < 0) t_restante = 0;
+
+  Serial.printf("Tensão: %.3f V | Corrente: %.3f mA | Tempo restante: %.2f s\n",
+                tensao, corrente_mA, t_restante);
+
+  delay(5000);
 }
