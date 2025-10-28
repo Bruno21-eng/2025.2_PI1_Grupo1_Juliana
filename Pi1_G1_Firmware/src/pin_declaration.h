0eef28efddcfffcfa2f91801b11976ddd1299c76
rodrigokborba <89036954+rodrigokborba@users.noreply.github.com>
2025-10-28 11:27:56 -0300

    Adiciona arquivo pin_declaration.h para os pinos do carrinho

---
+++ Pi1_G1_Firmware/src/pin_declaration.h
@@ -0,0 +1,19 @@
+#include <Arduino.h>
+
+//----------------------------------------Pins declarations---------------------------------
+//Encoders
+static const uint8_t ENC1_R = 17;
+static const uint8_t ENC2_L = 16;
+//Ponte H / Motores
+static const uint8_t PWMA_R = 32;
+static const uint8_t AIN2_R = 33;
+static const uint8_t AIN1_R = 25;
+static const uint8_t STBY = 26;
+static const uint8_t BIN2_L = 27;
+static const uint8_t BIN1_L = 14;
+static const uint8_t PWMB_L = 13;
+//Servo
+static const uint8_t PWM_SERVO = 18;
+//I2C
+static const uint8_t SDA = 21;
+static const uint8_t SCL = 22;
